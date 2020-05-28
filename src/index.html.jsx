fbabc4b50b313ec5ba721cf6b51ce3b86efcd15c
Price Comstock <price.comstock@gmail.com>
2020-05-27 20:09:05 -0400

    switched to a sick retro theme

--- src/index.html.jsx
+++ src/index.html.jsx
@@ -8,6 +8,7 @@ export default ({ data, environment }) => {
         <meta charSet="utf-8" />
         <title>Price Comstock</title>
         <link rel="stylesheet" href="./css/main.css" />
+        <script src="/js/index.js"></script>
 
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </head>
