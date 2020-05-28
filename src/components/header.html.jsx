12fbfa5f9e947da62367d9598915971ab155ed86
Price Comstock <price.comstock@gmail.com>
2020-05-27 21:03:29 -0400

    fixed header on mobile

--- src/components/header.html.jsx
+++ src/components/header.html.jsx
@@ -2,7 +2,7 @@ export default () => {
   return (
     <div className="header">
       <div>
-        <h1 className="serif">Hi, I'm Price Comstock</h1>
+        <h1>Hi, I'm Price Comstock</h1>
       </div>
       <div>
         <p>I'm a full stack developer. Here are some things I've built</p>
